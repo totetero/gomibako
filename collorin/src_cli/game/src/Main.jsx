bf579bf46700c44e672478047844dcad334607ee
A13005 <A13005@PC-7882.local>
2013-10-19 18:11:49 +0900

    画像登録の関数をわける

--- collorin/src_cli/game/src/Main.jsx
+++ collorin/src_cli/game/src/Main.jsx
@@ -17,25 +17,31 @@ class Main{
 			var jdat = JSON.parse(xhr.responseText);
 			// 画像準備
 			Main.imgs = {} : Map.<HTMLImageElement>;
-			var b64imgs = jdat["imgs"] as Map.<string>;
-			var count = 0;
-			for(var i in b64imgs){count++;}
-			for(var i in b64imgs){
-				var img = dom.createElement("img") as HTMLImageElement;
-				img.onload = function(e : Event){
-					if(--count == 0){
-						// すべての画像準備が終わったら処理開始
-						var canvas = dom.document.createElement("canvas") as HTMLCanvasElement;
-						var context = canvas.getContext("2d") as CanvasRenderingContext2D;
-						dom.document.body.appendChild(canvas);
-						context.drawImage(Main.imgs["player"], 0, 0);
-					}
-				};
-				img.src = b64imgs[i];
-				Main.imgs[i] = img;
-			}
+			Main.regImg(jdat["imgs"] as Map.<string>, function(){
+				// テスト
+				var canvas = dom.document.createElement("canvas") as HTMLCanvasElement;
+				var context = canvas.getContext("2d") as CanvasRenderingContext2D;
+				dom.document.body.appendChild(canvas);
+				context.drawImage(Main.imgs["player"], 0, 0);
+			});
 		});
 		xhr.send();
+	}
+
+	// ----------------------------------------------------------------
+	// base64情報配列から画像登録
+	static function regImg(b64imgs : Map.<string>, callback : function():void) : void{
+		var count = 0;
+		for(var i in b64imgs){count++;}
+		for(var i in b64imgs){
+			var img = dom.createElement("img") as HTMLImageElement;
+			img.onload = function(e : Event){
+				// すべての登録が終わったらコールバック
+				if(--count == 0){callback();}
+			};
+			img.src = b64imgs[i];
+			Main.imgs[i] = img;
+		}
 	}
 }
 
