87627efdde9c520c2cca77085d09c77033ac90d4
A13005 <A13005@PC-7882.local>
2013-10-22 10:32:09 +0900

    カメラ位置とか

--- collorin/src_cli/chat/jsx/Main.jsx
+++ collorin/src_cli/chat/jsx/Main.jsx
@@ -12,12 +12,9 @@ import 'Character.jsx';
 // メインクラス
 class Main{
 	static var imgs : Map.<HTMLImageElement>;
+	static var field : Field;
+	static var player : Player[];
 	static var clist : DrawUnit[];
-	static var player : DrawPlayer;
-
-	// フィールド画像
-	static var canvas : HTMLCanvasElement;
-	static var context : CanvasRenderingContext2D;
 
 	// ----------------------------------------------------------------
 	// main関数
@@ -40,52 +37,29 @@ class Main{
 	// ----------------------------------------------------------------
 	// 初期化
 	static function init() : void{
+		Main.field = new Field();
 		Main.clist = new DrawUnit[];
-		Main.clist.push(Main.player = new DrawPlayer(Main.imgs["player"]));
-
-		// フィールド画像作成準備
-		Main.canvas = dom.window.document.createElement("canvas") as HTMLCanvasElement;
-		Main.context = Main.canvas.getContext("2d") as CanvasRenderingContext2D;
-		var r = Main.canvas.width = Main.canvas.height = 500;
-		// フィールド画像作成
-		for(var i = 0; i < 2; i++){
-			for(var j = 0; j < 20; j++){
-				var pos = 5 + (r - 10) * j / 19;
-				Main.context.beginPath();
-				if(i == 0){Main.context.moveTo(5, pos); Main.context.lineTo(r - 5, pos);}
-				if(i == 1){Main.context.moveTo(pos, 5); Main.context.lineTo(pos, r - 5);}
-				Main.context.closePath();
-				Main.context.stroke();
-			}
-		}
+		Main.player = new Player[];
+		Main.player.push(new Player());
 	}
 
 	// ----------------------------------------------------------------
-	// mainloop関数
+	// メインループ
 	static function mainloop() : void{
 		Ctrl.calc();
 		Ctrl.context.clearRect(0, 0, Ctrl.canvas.width, Ctrl.canvas.height);
 
-		// フィールド描画
-		Ctrl.context.save();
-		Ctrl.context.translate(Ctrl.canvas.width * 0.5, Ctrl.canvas.height * 0.5);
-		Ctrl.context.scale(Ctrl.scale, Ctrl.scale * Ctrl.sinh);
-		Ctrl.context.rotate(Ctrl.rotv);
-		var x = 0;
-		var y = 0;
-		Ctrl.context.drawImage(Main.canvas, -x, -y);
-		Ctrl.context.restore();
+		var x = 100;
+		var y = 100;
 
+		// フィールド描画
+		Main.field.draw(x, y);
 		// プレイヤー描画準備
-		var x = 0;
-		var y = 0;
-		var z = 0;
-		var r = Math.PI / 180 * 90;
-		Main.player.preDraw(x, y, z, r, 1.2);
-		Main.player.setPose(0);
+		for(var i = 0; i < Main.player.length; i++){Main.player[i].preDraw(x, y);}
 		// キャラクター描画
 		DrawUnit.drawList(Main.clist);
 
+		// ループ
 		Timer.setTimeout(Main.mainloop, 33);
 	}
 
@@ -103,6 +77,80 @@ class Main{
 			img.src = b64imgs[i];
 			Main.imgs[i] = img;
 		}
+	}
+}
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+// 地面クラス
+class Field{
+	var canvas : HTMLCanvasElement;
+	var context : CanvasRenderingContext2D;
+
+	// ----------------------------------------------------------------
+	// コンストラクタ
+	function constructor(){
+		// キャンバス作成
+		this.canvas = dom.window.document.createElement("canvas") as HTMLCanvasElement;
+		this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+		var r = this.canvas.width = this.canvas.height = 500;
+		var divide = 20;
+		var margin = 5;
+		// フィールド画像作成
+		for(var i = 0; i < 2; i++){
+			for(var j = 0; j < divide; j++){
+				var pos = margin + (r - margin * 2) * j / (divide - 1);
+				this.context.beginPath();
+				if(i == 0){this.context.moveTo(margin, pos); this.context.lineTo(r - margin, pos);}
+				if(i == 1){this.context.moveTo(pos, margin); this.context.lineTo(pos, r - margin);}
+				this.context.closePath();
+				this.context.stroke();
+			}
+		}
+		// 画像枠線作成
+		this.context.beginPath();
+		this.context.moveTo(0, 0);
+		this.context.lineTo(0, r);
+		this.context.lineTo(r, r);
+		this.context.lineTo(r, 0);
+		this.context.closePath();
+		this.context.stroke();
+	}
+
+	// ----------------------------------------------------------------
+	// 描画
+	function draw(x : number, y : number) : void{
+		Ctrl.context.save();
+		Ctrl.context.translate(Ctrl.canvas.width * 0.5, Ctrl.canvas.height * 0.5);
+		Ctrl.context.scale(Ctrl.scale, Ctrl.scale * Ctrl.sinh);
+		Ctrl.context.rotate(Ctrl.rotv);
+		Ctrl.context.drawImage(this.canvas, -x, -y);
+		Ctrl.context.restore();
+	}
+}
+
+// プレイヤークラス
+class Player{
+	var drawCharacter : DrawPlayer;
+	var x = 0;
+	var y = 0;
+	var z = 0;
+	var r = Math.PI / 180 * 90;
+
+	// ----------------------------------------------------------------
+	// コンストラクタ
+	function constructor(){
+		this.drawCharacter = new DrawPlayer(Main.imgs["player"]);
+		Main.clist.push(this.drawCharacter);
+	}
+
+	// ----------------------------------------------------------------
+	// 描画準備
+	function preDraw(x : number, y : number) : void{
+		this.drawCharacter.preDraw(this.x - x, this.y - y, this.z, this.r, 1.2);
+		this.drawCharacter.setPose(0);
 	}
 }
 
