c4f0a4537685c4151cc762a0d82b0ee68c5c6694
A13005 <A13005@PC-7882.local>
2013-10-22 13:16:58 +0900

    タッチ座標位置計算とか

--- collorin/src_cli/chat/jsx/Main.jsx
+++ collorin/src_cli/chat/jsx/Main.jsx
@@ -95,13 +95,13 @@ class Field{
 		// キャンバス作成
 		this.canvas = dom.window.document.createElement("canvas") as HTMLCanvasElement;
 		this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
-		var r = this.canvas.width = this.canvas.height = 500;
+		var r = this.canvas.width = this.canvas.height = 510;
 		var divide = 20;
 		var margin = 5;
-		// フィールド画像作成
+		// 地形画像作成
 		for(var i = 0; i < 2; i++){
-			for(var j = 0; j < divide; j++){
-				var pos = margin + (r - margin * 2) * j / (divide - 1);
+			for(var j = 0; j < divide + 1; j++){
+				var pos = margin + (r - margin * 2) * j / divide;
 				this.context.beginPath();
 				if(i == 0){this.context.moveTo(margin, pos); this.context.lineTo(r - margin, pos);}
 				if(i == 1){this.context.moveTo(pos, margin); this.context.lineTo(pos, r - margin);}
@@ -122,11 +122,30 @@ class Field{
 	// ----------------------------------------------------------------
 	// 描画
 	function draw(x : number, y : number) : void{
+		// 地形描画
 		Ctrl.context.save();
 		Ctrl.context.translate(Ctrl.canvas.width * 0.5, Ctrl.canvas.height * 0.5);
 		Ctrl.context.scale(Ctrl.scale, Ctrl.scale * Ctrl.sinh);
 		Ctrl.context.rotate(Ctrl.rotv);
-		Ctrl.context.drawImage(this.canvas, -x, -y);
+		Ctrl.context.translate(-x, -y);
+		Ctrl.context.drawImage(this.canvas, 0, 0);
+
+		if(Ctrl.mdn){
+			// マウス座標から座標の獲得
+			var c = Math.cos(Ctrl.rotv);
+			var s = Math.sin(Ctrl.rotv);
+			var x0 = (Ctrl.mx - Ctrl.canvas.width * 0.5) / Ctrl.scale;
+			var y0 = (Ctrl.my - Ctrl.canvas.height * 0.5) / (Ctrl.scale * Ctrl.sinh);
+			var mx = (x0 *  c + y0 * s) + x;
+			var my = (x0 * -s + y0 * c) + y;
+			// タッチ座標位置描画
+			Ctrl.context.fillStyle = "rgba(0, 0, 0, 0.5)";
+			Ctrl.context.beginPath();
+			Ctrl.context.arc(mx, my, 6, 0, Math.PI*2, false);
+			Ctrl.context.fill();
+		}
+
+		// 描画終了
 		Ctrl.context.restore();
 	}
 }
@@ -134,9 +153,8 @@ class Field{
 // プレイヤークラス
 class Player{
 	var drawCharacter : DrawPlayer;
-	var x = 0;
-	var y = 0;
-	var z = 0;
+	var x = 100;
+	var y = 100;
 	var r = Math.PI / 180 * 90;
 
 	// ----------------------------------------------------------------
@@ -149,7 +167,7 @@ class Player{
 	// ----------------------------------------------------------------
 	// 描画準備
 	function preDraw(x : number, y : number) : void{
-		this.drawCharacter.preDraw(this.x - x, this.y - y, this.z, this.r, 1.2);
+		this.drawCharacter.preDraw(this.x - x, this.y - y, 0, this.r, 1.2);
 		this.drawCharacter.setPose(0);
 	}
 }
