b5fb7f598c07d178f48f5348c23b6d871be34686
totetero <tote@totalbeat-2.local>
2014-01-13 13:17:48 +0900

    クライアント側の構造改革準備 makefile書き換え

---
+++ collorin/src_cli_test/util/Loader.jsx
@@ -0,0 +1,61 @@
+import 'js.jsx';
+import 'js/web.jsx';
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+// メインクラス
+class Loader{
+	// 画像リスト
+	static var imgs : Map.<HTMLImageElement>;
+	static var b64imgs : Map.<string>;
+
+	// ----------------------------------------------------------------
+	// base64情報配列から画像登録
+	static function regImg(b64imgs : Map.<string>, callback : function():void) : void{
+		var count = 0;
+		for(var i in b64imgs){count++;}
+		if(count > 0){
+			for(var i in b64imgs){
+				if(i.search(/^b64_/) == 0){
+					// css用画像
+					Loader.b64imgs[i.substring(4)] = b64imgs[i];
+					if(--count == 0){callback();}
+				}else{
+					// canvas用画像
+					var img = dom.createElement("img") as HTMLImageElement;
+					img.onload = function(e : Event){
+						// すべての登録が終わったらコールバック
+						if(--count == 0){callback();}
+					};
+					img.src = b64imgs[i];
+					Loader.imgs[i] = img;
+				}
+			}
+		}else{
+			callback();
+		}
+	}
+
+	// ----------------------------------------------------------------
+	// XMLhttpリクエスト送信
+	static function loadxhr(url : string, request : string, successFunc : function(:string):void, failureFunc : function():void) : void{
+		// リクエスト開始準備
+		var xhr = new XMLHttpRequest();
+		if(request != ""){xhr.open("POST", url, true);}else{xhr.open("GET", url, true);}
+		// リクエスト正常終了
+		xhr.addEventListener("load", function(e : Event) : void{successFunc(xhr.responseText);});
+		// リクエスト失敗
+		xhr.addEventListener("abort", function(e : Event) : void{log "abort"; failureFunc();});
+		xhr.addEventListener("error", function(e : Event) : void{log "error"; failureFunc();});
+		xhr.addEventListener("timeout", function(e : Event) : void{log "timeout"; failureFunc();});
+		// 通信開始
+		xhr.send(request);
+	}
+}
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
---
+++ collorin/src_srv/Main_test.jsx
@@ -0,0 +1,77 @@
+import "require/nodejs.jsx";
+import "require/express.jsx";
+import "require/mongo.jsx";
+import "require/passport.jsx";
+
+import "pages/*.jsx";
+
+import "data/CharacterDrawInfo.jsx";
+
+// メインクラス
+class _Main{
+	// ----------------------------------------------------------------
+	// main関数
+	static function main(args : string[]) : void{
+		// データ初期化
+		CharacterDrawInfo.init();
+
+		// データベース接続
+		mongoose.connect("mongodb://localhost/totetest");
+		var mongoStore = new MongoStore({db: mongoose.connections[0]["db"]});
+
+		// expressサーバ設定
+		var app = express.create();
+		var srv = http.Server(app);
+		app.configure(function(){
+			app.set("secretKey", "totalbeat");
+			app.set("views", node.__dirname + "/src_cli_test"); // TEST app.set("views", node.__dirname + "/src_cli");
+			app.set("view engine", "ejs");
+			app.use(express.logger("dev"));
+			app.use(express.json());
+			app.use(express.urlencoded());
+			app.use(express.methodOverride());
+			app.use(express.cookieParser());
+			app.use(express.session({
+				secret: app.get("secretKey"),
+				store: mongoStore,
+				cookie: {httpOnly: false, maxAge: 30 * 24 * 60 * 60 * 1000}
+			}));
+			app.use(passport.initialize());
+			app.use(passport.session());
+			app.use(express.compress());
+			app.use(app.router);
+			app.use(express.static_(node.__dirname + "/public"));
+			app.use(express.errorHandler({dumpExceptions: true, showStack: true}));
+			//app.use(function(req : ExRequest, res : ExResponse, next : function():void){
+			//	res.status(404).render("404.ejs", null);
+			//});
+		});
+
+		// passport認証設定
+		AuthPage.setPassport();
+
+		// TESTココカラ --------
+		if(true){
+			app.get("/", function(req : ExRequest, res : ExResponse, next : function():void) : void{
+				res.render("index.ejs");
+			});
+			srv.listen(10080);
+			log "TEST MODE";
+			return;
+		}
+		// -------- TESTココマデ
+
+		// 各ページ設定
+		AuthPage.setPage(app);
+		GamePage.setPage(app);
+		TopPage.setPage(app);
+		MyPage.setPage(app);
+		StagePage.setPage(app);
+		ChatPage.setPage(app);
+		ChatPage.setSocket(app, srv, mongoStore);
+
+		srv.listen(10080);
+		log "Server running at http://127.0.0.1:10080/";
+	}
+}
+
