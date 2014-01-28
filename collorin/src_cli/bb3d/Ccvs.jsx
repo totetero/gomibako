01ce6618d68d9be10b3ed3b08e8176024cc12281
totetero <tote@totalbeat-2.local>
2014-01-29 02:47:32 +0900

    とりあえずキャンバスキャラクター追加

--- collorin/src_cli/bb3d/Ccvs.jsx
+++ collorin/src_cli/bb3d/Ccvs.jsx
@@ -18,11 +18,23 @@ class Ccvs{
 	var mdn : boolean;
 	var mx : int;
 	var my : int;
+	// ゲーム画面キャンバス カメラ位置
+	var cx : int;
+	var cy : int;
+	// ゲーム画面キャンバス 画面拡大回転
+	var scale : number = 2;
+	var rotv : number = Math.PI / 180 * 30;
+	var roth : number = Math.PI / 180 * 45;
+	var sinv : number;
+	var cosv : number;
+	var sinh : number;
+	var cosh : number;
 
 	// コンストラクタ
 	function constructor(width : int, height : int, canvas : HTMLCanvasElement){
 		this.width = width;
 		this.height = height;
+		// DOM獲得
 		this.canvas = canvas;
 		this.context = this.canvas.getContext("2d") as CanvasRenderingContext2D;
 		// ピクセルレシオ設定
@@ -35,6 +47,12 @@ class Ccvs{
 			this.canvas.height = Math.floor(this.height * PixelRatio);
 			this.context.scale(PixelRatio, PixelRatio);
 		}
+
+		// パラメーター初期化
+		this.sinv = Math.sin(this.rotv);
+		this.cosv = Math.cos(this.rotv);
+		this.sinh = Math.sin(this.roth);
+		this.cosh = Math.cos(this.roth);
 	}
 
 	// 破棄
@@ -47,6 +65,10 @@ class Ccvs{
 // キャンバスコントローラーカートリッジ 継承して使う
 abstract class SECctrlCanvas extends EventCartridge{
 	var ccvs : Ccvs;
+	// 内部演算用 マウス移動量差分を求める変数
+	var _tempmdn : boolean;
+	var _tempmx : int = 0;
+	var _tempmy : int = 0;
 
 	// コンストラクタ
 	function constructor(ccvs : Ccvs){
@@ -60,8 +82,46 @@ abstract class SECctrlCanvas extends EventCartridge{
 	// 計算
 	override function calc() : boolean{
 		var box = this.ccvs.canvas.getBoundingClientRect();
-		this.ccvs.mx = Ctrl.mx + Ctrl.sx - box.left - box.width * 0.5;
-		this.ccvs.my = Ctrl.my + Ctrl.sy - box.top - box.height * 0.5;
+		this.ccvs.mx = Ctrl.mx + Ctrl.sx - box.left;
+		this.ccvs.my = Ctrl.my + Ctrl.sy - box.top;
+
+		// キャンバス内でクリック開始したかの確認
+		if(this._tempmdn != Ctrl.mdn){
+			this._tempmdn = Ctrl.mdn;
+			this.ccvs.mdn = (this._tempmdn && 0 < this.ccvs.mx && this.ccvs.mx < this.ccvs.width && 0 < this.ccvs.my && this.ccvs.my < this.ccvs.height);
+		}
+
+		if(this.ccvs.mdn && Ctrl.mmv){
+			// 舞台回転処理
+			var x0 = this._tempmx - this.ccvs.width * 0.5;
+			var y0 = this._tempmy - this.ccvs.height * 0.5;
+			var r0 = Math.sqrt(x0 * x0 + y0 * y0);
+			var x1 = this.ccvs.mx - this.ccvs.width * 0.5;
+			var y1 = this.ccvs.my - this.ccvs.height * 0.5;
+			var r1 = Math.sqrt(x1 * x1 + y1 * y1);
+			if(r0 > 20 && r1 > 20){
+				var cos = (x0 * x1 + y0 * y1) / (r0 * r1);
+				if(cos > 1){cos = 1;}else if(cos < -1){cos = -1;}
+				this.ccvs.rotv += Math.acos(cos) * ((x0 * y1 - y0 * x1 > 0) ? 1 : -1);
+				// 垂直角度処理
+				this.ccvs.sinv = Math.sin(this.ccvs.rotv);
+				this.ccvs.cosv = Math.cos(this.ccvs.rotv);
+			}
+		}
+		this._tempmx = this.ccvs.mx;
+		this._tempmy = this.ccvs.my;
+
+		// 水平角度
+		var drh = this.ccvs.roth - Math.PI / 180 * 30;
+		if(Math.abs(drh) > 0.01){
+			this.ccvs.roth -= drh * 0.1;
+			this.ccvs.sinh = Math.sin(this.ccvs.roth);
+			this.ccvs.cosh = Math.cos(this.ccvs.roth);
+		}
+		// 拡大縮小
+		var scale = 1;
+		this.ccvs.scale += (scale - this.ccvs.scale) * 0.1;
+
 		return true;
 	}
 
--- collorin/src_cli/page/game/GamePage.jsx
+++ collorin/src_cli/page/game/GamePage.jsx
@@ -4,6 +4,7 @@ import "../../util/Loader.jsx";
 import "../../util/EventCartridge.jsx";
 import "../../util/Ctrl.jsx";
 import "../../bb3d/Ccvs.jsx";
+import "../../bb3d/Character.jsx";
 import "../Page.jsx";
 
 // ----------------------------------------------------------------
@@ -18,6 +19,10 @@ class GamePage extends Page{
 
 	// キャンバス情報
 	var ccvs : Ccvs;
+	// キャラクター
+	var player : GameCharacter;
+	var clist : DrawUnit[] = new DrawUnit[];
+	var slist : DrawUnit[] = new DrawUnit[];
 
 	// コンストラクタ
 	function constructor(){
@@ -36,7 +41,7 @@ class GamePage extends Page{
 	override function init() : void{
 		// テスト
 		Loader.loadImg({player: "img/character/player0/dot.png"}, function() : void{
-			log Loader.imgs;
+			this.player = new GameCharacter(this);
 		}, function():void{});
 
 		this.serialPush(new SECtransitionsPage(this));
@@ -45,9 +50,31 @@ class GamePage extends Page{
 
 	// キャンバス描画
 	function canvasDraw() : void{
-		// test
-		this.ccvs.context.fillRect(0, 0, this.ccvs.width, this.ccvs.height);
-		this.ccvs.context.drawImage(Loader.imgs["player"], 10, 10, 64, 64);
+		this.ccvs.context.clearRect(0, 0, this.ccvs.width, this.ccvs.height);
+		this.drawTestField(this.ccvs);
+		if(this.player != null){this.player.preDraw(this.ccvs);}
+		DrawUnit.drawList(this.ccvs, this.slist);
+		DrawUnit.drawList(this.ccvs, this.clist);
+	}
+
+	// テスト地形描画
+	function drawTestField(ccvs : Ccvs) : void{
+		// 描画開始
+		ccvs.context.save();
+		ccvs.context.translate(ccvs.width * 0.5, ccvs.height * 0.5);
+		ccvs.context.scale(ccvs.scale, ccvs.scale * ccvs.sinh);
+		ccvs.context.rotate(ccvs.rotv);
+		ccvs.context.translate(-this.ccvs.cx, -this.ccvs.cy);
+		// 描画
+		ccvs.context.beginPath();
+		ccvs.context.moveTo(-50, -50);
+		ccvs.context.lineTo(-50,  50);
+		ccvs.context.lineTo( 50,  50);
+		ccvs.context.lineTo( 50, -50);
+		ccvs.context.closePath();
+		ccvs.context.stroke();
+		// 描画終了
+		ccvs.context.restore();
 	}
 
 	// 破棄
@@ -73,6 +100,7 @@ class SECgamePageMain extends SECctrlCanvas{
 	// 計算
 	override function calc() : boolean{
 		super.calc();
+		this.page.player.calc(this.ccvs);
 		return true;
 	}
 
@@ -83,6 +111,70 @@ class SECgamePageMain extends SECctrlCanvas{
 
 	// 破棄
 	override function dispose() : void{
+	}
+}
+
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+// ----------------------------------------------------------------
+
+// キャラクタークラス
+class GameCharacter{
+	var _character : DrawCharacter;
+	var _shadow : DrawShadow;
+	var x : number;
+	var y : number;
+	var r : number;
+	var motion : string;
+	var action : int;
+
+	// ----------------------------------------------------------------
+	// コンストラクタ
+	function constructor(page : GamePage){
+		var drawInfo = new DrawInfo({"weapon":"whiteSword","parts":{"head":[[0.00,0.00,0.00,0,0,16,0],[-0.06,0.20,-0.02,0,48,16,0],[-0.06,-0.20,-0.02,0,48,16,1],[-0.19,0.00,-0.09,0,64,16,0],[-0.16,0.00,0.08,0,80,16,0],],"body":[[0.00,0.00,0.00,0,16,16,0]],"ftr1":[[0.00,0.00,0.00,0,32,8,0]],"ftl1":[[0.00,0.00,0.00,0,32,8,1]],"ftr2":[[0.00,0.00,0.00,32,32,8,0]],"ftl2":[[0.00,0.00,0.00,32,32,8,1]],"hndr":[[0.00,0.00,0.00,0,40,8,0]],"hndl":[[0.00,0.00,0.00,0,40,8,1]],},"pose":{"stand":[{"head":[1,0.00,0.00,0.52,45*0],"body":[1,-0.02,0.00,0.27,0],"ftr1":[1,0.02,0.10,0.10,0],"ftl1":[1,-0.02,-0.10,0.10,0],"hndr":[0,-0.02,0.20,0.25,0],"hndl":[0,0.02,-0.20,0.25,0],},],"walk":[{"head":[1,0.12,0.00,0.45,0],"body":[1,0.00,0.00,0.23,0],"ftr1":[1,0.10,0.07,0.10,0],"ftl2":[1,-0.20,-0.07,0.20,0],"hndr":[0,-0.10,0.15,0.25,0],"hndl":[0,0.10,-0.15,0.25,0],},{"head":[1,0.12,0.00,0.47,0],"body":[1,0.00,0.00,0.26,0],"ftr1":[1,0.00,0.07,0.10,0],"ftl1":[1,0.00,-0.07,0.15,0],"hndr":[0,-0.05,0.18,0.25,0],"hndl":[0,0.05,-0.18,0.25,0],},{"head":[1,0.12,0.00,0.45,0],"body":[1,0.00,0.00,0.23,0],"ftr2":[1,-0.20,0.07,0.20,0],"ftl1":[1,0.10,-0.07,0.10,0],"hndr":[0,0.10,0.15,0.25,0],"hndl":[0,-0.10,-0.15,0.25,0],},{"head":[1,0.12,0.00,0.47,0],"body":[1,0.00,0.00,0.26,0],"ftr1":[1,0.00,0.07,0.15,0],"ftl1":[1,0.00,-0.07,0.10,0],"hndr":[0,0.05,0.18,0.25,0],"hndl":[0,-0.05,-0.18,0.25,0],},],"dive":[{"head":[1,0.12,0.00,0.30,0],"body":[1,-0.02,0.00,0.20,0],"ftr2":[1,-0.18,0.07,0.10,0],"ftl2":[1,-0.18,-0.07,0.10,0],"hndr":[0,0.20,0.13,0.17,0],"hndl":[0,0.20,-0.13,0.17,0],},],"roll":[{"head":[3,-0.06,0.00,0.22,0],"body":[3,0.02,0.00,0.45,0],"ftr2":[4,-0.14,0.07,0.50,0],"ftl2":[4,-0.14,-0.07,0.50,0],"hndr":[0,-0.12,0.15,0.50,0],"hndl":[0,-0.12,-0.15,0.50,0],},{"head":[1,0.06,0.00,0.38,0],"body":[1,-0.02,0.00,0.15,0],"ftr2":[2,0.14,0.07,0.10,0],"ftl2":[2,0.14,-0.07,0.10,0],"hndr":[0,0.12,0.15,0.10,0],"hndl":[0,0.12,-0.15,0.10,0],},],"attack1":[{"head":[1,0.00,0.00,0.55,0],"body":[1,-0.02,0.00,0.30,0],"ftr1":[1,0.00,0.10,0.10,0],"ftl1":[1,0.00,-0.10,0.12,0],"hndr":[0,0.08,-0.05,0.28,0],"hndl":[0,0.00,-0.20,0.28,0],},{"head":[1,0.00,0.00,0.55,0],"body":[1,-0.02,0.00,0.30,0],"ftr1":[1,0.00,0.10,0.10,0],"ftl1":[1,0.00,-0.10,0.12,0],"hndr":[0,0.08,-0.05,0.28,0],"hndl":[0,0.00,-0.20,0.28,0],},{"head":[1,0.03,0.00,0.53,0],"body":[1,-0.01,0.00,0.28,0],"ftr1":[1,-0.02,0.10,0.10,0],"ftl1":[1,0.02,-0.10,0.15,0],"hndr":[0,0.06,-0.08,0.26,0],"hndl":[0,0.00,-0.20,0.26,0],"weapon":[0,0.6,0,0.25,0],},{"head":[1,0.05,0.00,0.51,0],"body":[1,0.01,0.00,0.26,0],"ftr1":[1,-0.04,0.09,0.10,0],"ftl1":[1,0.04,-0.09,0.15,0],"hndr":[0,0.08,-0.05,0.24,0],"hndl":[0,0.00,-0.20,0.24,0],"weapon":[1,0.6,0,0.25,0],},{"head":[1,0.08,0.00,0.50,0],"body":[1,0.03,0.00,0.25,0],"ftr1":[1,-0.08,0.08,0.10,0],"ftl1":[1,0.08,-0.08,0.10,0],"hndr":[0,0.12,0.15,0.23,0],"hndl":[0,0.00,-0.20,0.23,0],"weapon":[2,0.6,0,0.25,0],},{"head":[1,0.08,0.00,0.50,0],"body":[1,0.03,0.00,0.25,0],"ftr1":[1,-0.08,0.08,0.10,0],"ftl1":[1,0.08,-0.08,0.10,0],"hndr":[0,0.12,0.15,0.23,0],"hndl":[0,0.00,-0.20,0.23,0],"weapon":[2,0.6,0,0.25,0],},],"attack2":[{"head":[1,0.08,0.00,0.50,0],"body":[1,0.03,0.00,0.25,0],"ftr1":[1,-0.08,0.08,0.10,0],"ftl1":[1,0.08,-0.08,0.10,0],"hndr":[0,0.08,0.20,0.23,0],"hndl":[0,0.00,-0.20,0.23,0],"weapon":[3,0.6,0,0.25,0],},{"head":[1,0.08,0.00,0.50,0],"body":[1,0.03,0.00,0.25,0],"ftr1":[1,-0.08,0.08,0.10,0],"ftl1":[1,0.08,-0.08,0.10,0],"hndr":[0,0.08,0.20,0.23,0],"hndl":[0,0.00,-0.20,0.23,0],"weapon":[3,0.6,0,0.25,0],},{"head":[1,0.08,0.00,0.50,0],"body":[1,0.03,0.00,0.25,0],"ftr1":[1,-0.08,0.08,0.10,0],"ftl1":[1,0.08,-0.08,0.10,0],"hndr":[0,0.08,0.20,0.23,0],"hndl":[0,0.00,-0.20,0.23,0],"weapon":[3,0.6,0,0.25,0],},],"damage":[{"head":[1,0.00,0.00,0.45+0.25,0],"body":[1,-0.02,0.00,0.20+0.25,0],"ftr2":[2,0.12,0.10,0.10+0.25,0],"ftl2":[2,0.12,-0.10,0.10+0.25,0],"hndr":[0,0.02,0.20,0.28+0.25,0],"hndl":[0,0.02,-0.20,0.28+0.25,0],},{"head":[1,0.00,0.00,0.45+0.3,0],"body":[1,-0.02,0.00,0.20+0.3,0],"ftr2":[2,0.12,0.10,0.10+0.3,0],"ftl2":[2,0.12,-0.10,0.10+0.3,0],"hndr":[0,0.02,0.20,0.28+0.3,0],"hndl":[0,0.02,-0.20,0.28+0.3,0],},{"head":[1,0.00,0.00,0.45+0.25,0],"body":[1,-0.02,0.00,0.20+0.25,0],"ftr2":[2,0.12,0.10,0.10+0.25,0],"ftl2":[2,0.12,-0.10,0.10+0.25,0],"hndr":[0,0.02,0.20,0.28+0.25,0],"hndl":[0,0.02,-0.20,0.28+0.25,0],},{"head":[1,0.00,0.00,0.45+0.1,0],"body":[1,-0.02,0.00,0.20+0.1,0],"ftr2":[2,0.12,0.10,0.10+0.1,0],"ftl2":[2,0.12,-0.10,0.10+0.1,0],"hndr":[0,0.02,0.20,0.28+0.1,0],"hndl":[0,0.02,-0.20,0.28+0.1,0],},{"head":[1,0.00,0.00,0.45,0],"body":[1,-0.02,0.00,0.20,0],"ftr2":[2,0.12,0.10,0.10,0],"ftl2":[2,0.12,-0.10,0.10,0],"hndr":[0,0.02,0.20,0.28,0],"hndl":[0,0.02,-0.20,0.28,0],},{"head":[1,0.12,0.00,0.43,0],"body":[1,-0.02,0.00,0.22,0],"ftr1":[1,-0.02,0.10,0.10,0],"ftl1":[1,-0.02,-0.10,0.10,0],"hndr":[0,0.05,0.18,0.25,0],"hndl":[0,0.05,-0.18,0.25,0],},{"head":[1,0.12,0.00,0.43,0],"body":[1,-0.02,0.00,0.22,0],"ftr1":[1,-0.02,0.10,0.10,0],"ftl1":[1,-0.02,-0.10,0.10,0],"hndr":[0,0.05,0.18,0.25,0],"hndl":[0,0.05,-0.18,0.25,0],},{"head":[1,0.12,0.00,0.43,0],"body":[1,-0.02,0.00,0.22,0],"ftr1":[1,-0.02,0.10,0.10,0],"ftl1":[1,-0.02,-0.10,0.10,0],"hndr":[0,0.05,0.18,0.25,0],"hndl":[0,0.05,-0.18,0.25,0],},{"head":[1,0.12,0.00,0.43,0],"body":[1,-0.02,0.00,0.22,0],"ftr1":[1,-0.02,0.10,0.10,0],"ftl1":[1,-0.02,-0.10,0.10,0],"hndr":[0,0.05,0.18,0.25,0],"hndl":[0,0.05,-0.18,0.25,0],},],}});
+		var size = 1.2;
+		this._character = new DrawCharacter(Loader.imgs["player"], drawInfo, size);
+		this._shadow = new DrawShadow(size);
+		page.clist.push(this._character);
+		page.slist.push(this._shadow);
+		this.x = 0;
+		this.y = 0;
+		this.r = 0;
+	}
+
+	// ----------------------------------------------------------------
+	// 計算
+	function calc(ccvs : Ccvs) : void{
+		this.action++;
+		if     (Ctrl.krt && Ctrl.kup){this.r = Math.PI * 1.74 - ccvs.rotv;}
+		else if(Ctrl.klt && Ctrl.kup){this.r = Math.PI * 1.26 - ccvs.rotv;}
+		else if(Ctrl.klt && Ctrl.kdn){this.r = Math.PI * 0.74 - ccvs.rotv;}
+		else if(Ctrl.krt && Ctrl.kdn){this.r = Math.PI * 0.26 - ccvs.rotv;}
+		else if(Ctrl.krt){this.r = Math.PI * 0.00 - ccvs.rotv;}
+		else if(Ctrl.kup){this.r = Math.PI * 1.50 - ccvs.rotv;}
+		else if(Ctrl.klt){this.r = Math.PI * 1.00 - ccvs.rotv;}
+		else if(Ctrl.kdn){this.r = Math.PI * 0.50 - ccvs.rotv;}
+		else{this.action = 0;}
+		if(this.action > 0){
+			var speed = 3;
+			this.x += speed * Math.cos(this.r);
+			this.y += speed * Math.sin(this.r);
+			this.motion = "walk";
+		}else{
+			this.motion = "stand";
+		}
+	}
+
+	// ----------------------------------------------------------------
+	// 描画準備
+	function preDraw(ccvs : Ccvs) : void{
+		var x = this.x;
+		var y = this.y;
+		this._shadow.preDraw(ccvs, x, y, 0);
+		switch(this.motion){
+			case "walk": this._character.preDraw(ccvs, x, y, 0, this.r, "walk", ((this.action / 6) as int) % this._character.getLen("walk")); break;
+			default: this._character.preDraw(ccvs, x, y, 0, this.r, "stand", 0); break;
+		}
 	}
 }
 
