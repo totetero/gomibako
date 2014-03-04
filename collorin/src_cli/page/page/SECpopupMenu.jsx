dc72f3bbc1eab1dee12d005fdd34e10e2ab3742a
totetero <totetero@gmail.com>
2014-03-04 20:32:47 +0900

    バグとメニューレイアウト修正

--- collorin/src_cli/page/page/SECpopupMenu.jsx
+++ collorin/src_cli/page/page/SECpopupMenu.jsx
@@ -19,10 +19,9 @@ class SECpopupMenu extends SECpopup{
 			<div class="core-btn quest">クエスト</div>
 			<div class="core-btn chara">キャラクター</div>
 			<div class="core-btn item">アイテム</div>
-			<div class="core-btn status">ステータス</div>
 			<div class="core-btn friend">友達</div>
-			<div class="core-btn refbook">図鑑</div>
 			<div class="core-btn setting">設定</div>
+			<div class="core-btn refbook">図鑑</div>
 			<div class="core-btn help">ヘルプ</div>
 			<div class="core-btn close">閉じる</div>
 		</div>
@@ -51,10 +50,9 @@ class SECpopupMenu extends SECpopup{
 		this._btnList["quest"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn quest").item(0) as HTMLDivElement, true);
 		this._btnList["chara"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn chara").item(0) as HTMLDivElement, true);
 		this._btnList["item"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn item").item(0) as HTMLDivElement, true);
-		this._btnList["status"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn status").item(0) as HTMLDivElement, true);
 		this._btnList["friend"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn friend").item(0) as HTMLDivElement, true);
-		this._btnList["refbook"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn refbook").item(0) as HTMLDivElement, true);
 		this._btnList["setting"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn setting").item(0) as HTMLDivElement, true);
+		this._btnList["refbook"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn refbook").item(0) as HTMLDivElement, true);
 		this._btnList["help"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn help").item(0) as HTMLDivElement, true);
 		this._btnList["close"] = new PageButton(this.windowDiv.getElementsByClassName("core-btn close").item(0) as HTMLDivElement, true);
 		this._btnList["outer"] = new PageButton(this.windowDiv, false);
@@ -66,10 +64,10 @@ class SECpopupMenu extends SECpopup{
 		for(var name in this._btnList){this._btnList[name].calc(true);}
 
 		// コンテンツボタン
-		if(this._btnList["world"].trigger){Page.transitionsPage("world");}
-		if(this._btnList["quest"].trigger){Page.transitionsPage("quest");}
-		if(this._btnList["chara"].trigger){Page.transitionsPage("chara");}
-		if(this._btnList["item"].trigger){Page.transitionsPage("item");}
+		if(this._btnList["world"].trigger){this._btnList["world"].trigger = false; Page.transitionsPage("world");}
+		if(this._btnList["quest"].trigger){this._btnList["quest"].trigger = false; Page.transitionsPage("quest");}
+		if(this._btnList["chara"].trigger){this._btnList["chara"].trigger = false; Page.transitionsPage("chara");}
+		if(this._btnList["item"].trigger){this._btnList["item"].trigger = false; Page.transitionsPage("item");}
 
 		// 閉じるボタン
 		if(this._btnList["close"].trigger || this._btnList["outer"].trigger){
