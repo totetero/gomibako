19037772207597c5ed744539a95cb2f9c56be2f4
totetero <totetero@gmail.com>
2014-04-02 03:57:36 +0900

    すごろく いったんnodeでもモックデータをそのまま使う

--- collorin/src_srv/page/dice/DicePage.jsx
+++ collorin/src_srv/page/dice/DicePage.jsx
@@ -6,6 +6,9 @@ import "../../util/ImageServer.jsx";
 import "../../models/User.jsx";
 import "../../data/CharacterDrawInfo.jsx";
 
+// test
+import "../../../src_cli/util/mock/MockDice.jsx";
+
 // マイページ
 class DicePage{
 	static var _rcli : RedisClient;
@@ -18,74 +21,12 @@ class DicePage{
 
 		// -------- expressページ --------
 		app.post("/dice", function(req : ExRequest, res : ExResponse, next : function():void) : void{
-			DicePage._entry(function(jdat : Map.<variant>) : void{res.contentType("application/json").send(JSON.stringify(jdat));});
+			// test
+			MockDice.loadxhr("/dice", req.body, function(response : variant) : void{
+				response["imgs"] = ImageServer.convertAddress(response["imgs"] as Map.<string>);
+				res.contentType("application/json").send(JSON.stringify(response));
+			});
 		});
-	}
-
-	// ----------------------------------------------------------------
-	// ゲーム開始
-	static function _entry(callback : function(jdat:Map.<variant>):void) : void{
-		var jdat = {} : Map.<variant>;
-		var imgs = {} : Map.<string>;
-
-		// フィールド情報
-		jdat["hex"] = [
-			{x: 0, y: 3, type: 1},
-			{x: 0, y: 4, type: 1},
-			{x: 0, y: 5, type: 2},
-			{x: 1, y: 5, type: 1},
-			{x: 2, y: 4, type: 1},
-			{x: 2, y: 3, type: 1},
-			{x: 2, y: 2, type: 1},
-			{x: 1, y: 2, type: 1},
-			{x: 3, y: 4, type: 1},
-			{x: 4, y: 3, type: 1},
-			{x: 5, y: 3, type: 1},
-			{x: 5, y: 4, type: 1},
-			{x: 5, y: 5, type: 1},
-			{x: 5, y: 6, type: 1},
-			{x: 4, y: 7, type: 1},
-			{x: 3, y: 7, type: 1},
-			{x: 2, y: 7, type: 1},
-			{x: 1, y: 7, type: 1},
-			{x: 1, y: 6, type: 1},
-			{x: 3, y: 1, type: 1},
-			{x: 4, y: 0, type: 1},
-			{x: 5, y: 0, type: 1},
-			{x: 5, y: 1, type: 1},
-			{x: 4, y: 2, type: 1},
-		];
-
-		// さいころ画像
-		imgs["img_dice"] = "/img/dice/test.png";
-
-		// キャラクター情報
-		var charaInfoList = [[
-			// プレイヤー情報
-			{code: "player1", drawInfo: CharacterDrawInfo.data["human"], size: 1.2, x: 1, y : 7, r: Math.PI * 1.5},
-			{code: "player2", drawInfo: CharacterDrawInfo.data["human"], size: 1.2, x: 2, y : 7, r: Math.PI * 1.5},
-		],[
-			// 敵情報
-			{code: "enemy1", drawInfo: CharacterDrawInfo.data["human"], size: 1.2, x: 2, y : 4, r: Math.PI * 0.5},
-		]];
-
-		// 初期カメラ位置
-		jdat["camera"] = [charaInfoList[0][0]["x"], charaInfoList[0][0]["y"]];
-
-		// キャラクター情報の画像読み込み
-		for(var i = 0; i < charaInfoList.length; i++){
-			for(var j = 0; j < charaInfoList[i].length; j++){
-				var code = charaInfoList[i][j]["code"] as string;
-				imgs["img_dot_" + code] = "/img/character/" + code + "/dot.png";
-				imgs["css_icon_" + code] = "/img/character/" + code + "/icon.png";
-				imgs["css_bust_" + code] = "/img/character/" + code + "/bust.png";
-				imgs["css_damage_" + code] = "/img/character/" + code + "/damage.png";
-			}
-		}
-
-		jdat["charaInfo"] = charaInfoList;
-		jdat["imgs"] = ImageServer.convertAddress(imgs);
-		callback(jdat);
 	}
 }
 
