e2afbb4a6860e2bfe66969b4dd0a6ceb6fffa660
totetero <totetero@gmail.com>
2014-05-27 13:22:30 +0900

    ローディングの修正

--- collorin/src_cli/util/Loading.jsx
+++ collorin/src_cli/util/Loading.jsx
@@ -6,9 +6,11 @@ import "js/web.jsx";
 
 // ローディング表示クラス
 class Loading{
+	static const _maxOpacity = 5;
 	static var _div : HTMLDivElement;
 	static var _count = 0;
 	static var _action = 0;
+	static var _opacity = 0;
 
 	// ----------------------------------------------------------------
 	// 初期化
@@ -20,15 +22,28 @@ class Loading{
 	// 計算
 	static function calc() : void{
 		// ローディング処理
-		if(Loading._count > 0 || (5 <= Loading._action && Loading._action < 30)){
-			var action = Loading._action - 5;
-			if(action == 0){Loading._div.style.display = "block";}
-			switch(action % 40){
+		if(Loading._count > 0 || Loading._opacity > 0){
+			if(Loading._action == 0){Loading._div.style.display = "block";}
+
+			// 透明度
+			if(Loading._count > 0){
+				if(Loading._action <= Loading._maxOpacity){
+					Loading._opacity = Loading._action;
+					Loading._div.style.opacity = (Loading._opacity / Loading._maxOpacity) as string;
+				}
+			}else{
+				Loading._opacity--;
+				Loading._div.style.opacity = (Loading._opacity / Loading._maxOpacity) as string;
+			}
+
+			// アニメーション
+			switch(Loading._action % 40){
 				case  0: Loading._div.setAttribute("txt", "loading"); break;
 				case 10: Loading._div.setAttribute("txt", "loading."); break;
 				case 20: Loading._div.setAttribute("txt", "loading.."); break;
 				case 30: Loading._div.setAttribute("txt", "loading..."); break;
 			}
+
 			Loading._action++;
 		}else if(Loading._action != 0){
 			Loading._div.style.display = "none";
