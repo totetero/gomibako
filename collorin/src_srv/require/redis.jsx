80b17b576d6c41c970037649360c032401b11b1b
totetero <totetero@gmail.com>
2014-06-23 00:41:09 +0900

    redis 関数追加

--- collorin/src_srv/require/redis.jsx
+++ collorin/src_srv/require/redis.jsx
@@ -14,7 +14,16 @@ native class RedisClient{
 
 	function sadd(value : string[], callback : function(err:variant,result:Nullable.<string>):void) : void;
 	function srem(value : string[], callback : function(err:variant,result:Nullable.<string>):void) : void;
+	function scard(value : string[], callback : function(err:variant,result:Nullable.<string>):void) : void;
 	function smembers(value : string[], callback : function(err:variant,results:string[]):void) : void;
+
+	function hset(value : string[], callback : function(err:variant,result:Nullable.<string>):void) : void;
+	function hget(value : string[], callback : function(err:variant,result:Nullable.<string>):void) : void;
+	function hmset(key : string, json : variant, callback : function(err:variant,result:Nullable.<string>):void) : void;
+	function hmget(key : string, field : string[], callback : function(err:variant,results:string[]):void) : void;
+	function hkeys(key : string, callback : function(err:variant,results:string[]):void) : void;
+	function hvals(key : string, callback : function(err:variant,results:string[]):void) : void;
+	function hgetall(key : string, callback : function(err:variant,results:variant):void) : void;
 }
 
 native class RedisStore{
